e1e7382e6e520f88e4dfd37e849d87d6fdcb4be9
sebastienD <sebastien.descamps@gmail.com>
2017-12-15 23:12:51 +0100

    day 10

--- day10/src/Main_test.kt
+++ day10/src/Main_test.kt
@@ -3,11 +3,36 @@ import org.junit.Test as test
 
 class Main_testKt() {
 
-    @test fun f() {
+    @test fun `should rerverse list`() {
         val lengths: List<Int> = mutableListOf(3, 4, 1, 5)
+        val numbers = generateSequence(0, { it + 1 }).take(5).toMutableList()
+        val pair = round(lengths, numbers, Pair(0,0))
+        assertEquals(listOf(3,4,2,1,0) , numbers)
+        assertEquals(4 , pair.first)
+        assertEquals(4 , pair.second)
+    }
 
-        val actual = launch(lengths, 5)
+    @test fun `number to xor`() {
+        val v = 65.xor(27).xor(9).xor(1).xor(4)
+                .xor(3).xor(40).xor(50).xor(91)
+                .xor(7).xor(6).xor(0).xor(2)
+                .xor(5).xor(68).xor(22)
+        assertEquals(64 , v)
+    }
 
-        assertEquals(listOf(2,1,0,3,4) , actual)
+    @test fun `empty hash`() {
+        assertEquals("a2582a3a0e66e6e86e3812dcb672a272" , hash(""))
+    }
+
+    @test fun `AoC 2017 hash`() {
+        assertEquals("33efeb34ea91902bb2f59c9920caa6cd" , hash("AoC 2017"))
+    }
+
+    @test fun `1,2,3 hash`() {
+        assertEquals("3efbe78a8d82f29979031a4aa0b16a9d" , hash("1,2,3"))
+    }
+
+    @test fun `1,2,4 hash`() {
+        assertEquals("63960835bcdc130f0b66d7ff4f6a5a8e" , hash("1,2,4"))
     }
 }
--- day10/src/main.kt
+++ day10/src/main.kt
@@ -1,43 +1,71 @@
-import java.util.ArrayList
 
-fun launch(lengths: List<Int>, number: Int): List<Int> {
-    val numbers = generateSequence(0, { it + 1 }).take(number).toMutableList()
+fun round(listOfLength: List<Int>, numbers: MutableList<Int>, pair: Pair<Int, Int>): Pair<Int, Int> {
+    val size = numbers.size
 
-    var currentPosition = 0
-    var skipSize = 0
+    var currentPosition = pair.first
+    var skip = pair.second
 
-    for (i in 0..(number-1)) {
+    for (i in 0..(listOfLength.size-1)) {
         val indexMin = currentPosition
-        var length = lengths[i]
+        var length = listOfLength[i]
 
         println("indexMin: "+indexMin+" length: "+length)
 
-        val seq: List<Int> = generateSequence(indexMin, { (it + 1) % number }).take(length).toList()
-        println("seq: "+seq)
-        val sub = seq.map { it -> numbers[it] }.reversed()
-        println(sub)
+        val changedIndex = generateSequence(indexMin, { (it + 1) % size })
+                .take(length)
+                .toList()
 
-        var loop = 0
-        for (s in seq) {
-            numbers[s] = sub[loop]
-            loop++
+        val changedValues = changedIndex
+                .map { numbers[it] }
+                .reversed()
+
+        for (i in 0..(length - 1)) {
+            numbers[changedIndex[i]] = changedValues[i]
         }
 
-        currentPosition = (currentPosition + length + skipSize)%number
-        skipSize++
+        currentPosition = (currentPosition + length + skip)%size
+        skip++
 
-        println("end")
-        println(numbers)
+        println("current position: %s, skip: %s".format(currentPosition, skip))
     }
 
     println(numbers)
+    return Pair(currentPosition, skip)
+}
 
-    return numbers
+fun hash(toEncoded: String): String {
+    var numbers = generateSequence(0, { it + 1 }).take(256).toMutableList()
+
+    var lengths = toEncoded
+            .toCharArray()
+            .map { it.toInt() }
+            .toMutableList()
+    lengths.addAll(listOf(17,31,73,47,23))
+
+    var pair = Pair(0,0)
+    for (i in 1..64) {
+        pair = round(lengths, numbers, pair)
+    }
+
+    val sparseHash = numbers
+
+    var finalListe = ArrayList<Int>(16)
+    for (b in 0..15) {
+        var v = sparseHash[0 + b*16]
+        for (i in 1..15) {
+            v = v.xor(sparseHash[i + b*16])
+        }
+        finalListe.add(v)
+    }
+
+    return finalListe.joinToString("") { Integer.toHexString(it).padStart(2, '0') }
 }
 
 fun main(args: Array<String>) {
+    //val listOfLength = mutableListOf(34,88,2,222,254,93,150,0,199,255,39,32,137,136,1,167)
+    //val liste = launch(listOfLength, 256)
+    //println("le produit des 2 premières valeur est %s".format(liste[0] * liste[1]))
 
-    val lengths: List<Int> = mutableListOf(34,88,2,222,254,93,150,0,199,255,39,32,137,136,1,167)
-
-    launch(lengths, 256)
+    val hashValue = hash("34,88,2,222,254,93,150,0,199,255,39,32,137,136,1,167")
+    println(hashValue)
 }
