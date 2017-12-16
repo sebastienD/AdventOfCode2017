d65dc5a1fbfacef41b68e9cae835f9319f9c2ac8
sebastienD <sebastien.descamps@gmail.com>
2017-12-16 01:26:05 +0100

    day 15

--- day15/main.go
+++ day15/main.go
@@ -16,13 +16,21 @@ func generator(num int64, pA param, pB param) int {
 	prevB := pB.first
 	count := 0
 	for num > 0 {
-		prevA = (prevA * pA.factor) % 2147483647
+		for prevA%4 != 0 {
+			prevA = (prevA * pA.factor) % 2147483647
+		}
+		//fmt.Printf("prev A: %v\n", prevA)
 		binA := leftPad2Len(strconv.FormatInt(prevA, 2), "0", 16)
 		sA := binA[len(binA)-16:]
+		prevA = (prevA * pA.factor) % 2147483647
 
-		prevB = (prevB * pB.factor) % 2147483647
+		for prevB%8 != 0 {
+			prevB = (prevB * pB.factor) % 2147483647
+		}
+		//fmt.Printf("prev B: %v\n", prevB)
 		binB := leftPad2Len(strconv.FormatInt(prevB, 2), "0", 16)
 		sB := binB[len(binB)-16:]
+		prevB = (prevB * pB.factor) % 2147483647
 
 		if sA == sB {
 			count++
@@ -44,7 +52,7 @@ func leftPad2Len(s string, padStr string, overallLen int) string {
 }
 
 func main() {
-	result := generator(40000000, param{16807, 634}, param{48271, 301})
+	result := generator(5000000, param{16807, 634}, param{48271, 301})
 
 	fmt.Printf("result %v\n", result)
 }
--- day15/main_test.go
+++ day15/main_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestGenerator(t *testing.T) {
-	result := generator(40000000, param{16807, 65}, param{48271, 8921})
-	if result != 588 {
-		t.Fatalf("Expected %s but got %s", 588, result)
+	result := generator(5000000, param{16807, 65}, param{48271, 8921})
+	if result != 309 {
+		t.Fatalf("Expected %v but got %v", 588, result)
 	}
 }
